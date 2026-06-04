22d30b2da11e8b8751ccf65474cafc2589dc0d8e
JoaquinValdesret <joaquin.valdes2401@alumnos.ubiobio.cl>
2026-06-04 13:01:15 -0400

    Actualizo db.js

--- config/db.js
+++ config/db.js
@@ -11,7 +11,7 @@ const AppDataSource = new DataSource({
   synchronize: true, // Crea y actualiza las tablas automáticamente en base a tus entities
   logging: true,  // Muestra las consultas SQL en la consola
   entities: [
-    // Entidades base de tus compañeros (agrega las demás que tengan)
+    // Entidades
     require('../entities/Vivienda'),
     require('../entities/CuadrillaTrabajaEnVivienda'),
     require('../entities/Ciudad'),
