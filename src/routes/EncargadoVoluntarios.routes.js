4ede5b9b576d639fa4b82098a81c69c86157d71e
Sebastian <psebaaa.1@gmail.com>
2026-06-09 12:04:59 -0400

    mod en voluntario y encargado de voluntarios

--- src/routes/EncargadoVoluntarios.routes.js
+++ src/routes/EncargadoVoluntarios.routes.js
@@ -1,11 +1,12 @@
-import express from 'express';
+const express = require('express');
+const encargadoVoluntariosController = require('../controllers/EncargadoVoluntarios.controller');
+
 const router = express.Router();
 
-//import encargadoVoluntariosController from '../controllers/EncargadoVoluntarios.controller.js';
+router.get('/ListaPostulantes', encargadoVoluntariosController.obtenerListaPostulantes);
+router.get('/ListaVoluntarios', encargadoVoluntariosController.obtenerListaVoluntarios);
+router.get('/ListaPostulantes/:rut', encargadoVoluntariosController.obtenerPostulante);
+router.get('/ListaVoluntarios/:rut', encargadoVoluntariosController.obtenerVoluntario);
+router.patch('/ListaPostulantes/:rut/aprobar', encargadoVoluntariosController.aprobarIngresoPostulante);
 
-router.get('/ListaPostulantes',encargadoVoluntariosController.obtenerListaPostulantes);
-router.get('/ListaVoluntarios',encargadoVoluntariosController.obtenerListaVoluntarios);
-router.get('/ListaPostulantes/:id',encargadoVoluntariosController.obtenerPostulante);
-router.get('/ListaVoluntarios/:id',encargadoVoluntariosController.obtenerVoluntario);
-
-
+module.exports = router;
