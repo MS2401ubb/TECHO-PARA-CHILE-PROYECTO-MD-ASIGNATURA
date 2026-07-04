5cd9ff2fcdb004b8099468ed6f561f647b3a5f01
MS2401ubb <mauricio.sandoval2401@alumnos.ubiobio.cl>
2026-07-04 18:32:32 -0400

    usuario.entity, cambie nullables para facilitar creacion de voluntario espontaneo, en service 'roles validos' se separan entre Voluntario General y Voluntario Espontáneo ademas de Voluntario.

--- backend/src/entities/usuario.entity.js
+++ backend/src/entities/usuario.entity.js
@@ -1,58 +1,66 @@
-import { EntitySchema } from 'typeorm';
-
 export default new EntitySchema({
-    name: 'Usuario',
-    tableName: 'usuarios',
-    columns: {
-        rut: {
-            primary: true,
-            type: 'varchar',
-            length: 15,
-            nullable: false,
-        },
-        password:{
-            type: 'varchar',
-            length: 100,
-            nullable: false,
-        },
-        nombre:{
-            type: 'varchar',
-            length: 100,
-            nullable: false,
-        },
-        primerApellido:{
-            type: 'varchar',
-            length: 100,
-            nullable: false,
-        },
-        segundoApellido:{
-            type: 'varchar',
-            length: 100,
-            nullable: false,
-        },
-        fechaNacimiento:{
-            type: 'date',
-        },
-        email:{
-            type: 'varchar',
-            length: 200,
-        },
-        telefono:{
-            type: 'varchar',
-            length: 20,
-        },
-        rol:{
-            type:'varchar',
-            length: 50,
-            nullable: false,
-        },
+  name: 'Usuario',
+  tableName: 'usuarios',
+  columns: {
+    rut: {
+      primary: true,
+      type: 'varchar',
+      length: 15,
+      nullable: false,
     },
-    relations: {
-        ciudad: {
-            target: 'Ciudad',
-            type: 'many-to-one',
-            joinColumn:{ name: 'codigoCiudad'},
-            onDelete: 'RESTRICT',
-        },
+    password: {
+      type: 'varchar',
+      length: 100,
+      nullable: false,
     },
+    nombre: {
+      type: 'varchar',
+      length: 100,
+      nullable: false,
+    },
+    primerApellido: {
+      type: 'varchar',
+      length: 100,
+      nullable: false,
+    },
+    rol: {
+      type: 'varchar',
+      length: 50,
+      nullable: false, //   "Voluntario Espontáneo","Voluntario General", "Jefe de Cuadrilla", "Encargado de Voluntarios", "Encargado de Central", "admin"
+    },
+    telefono: {
+      type: 'varchar',
+      length: 20,
+      nullable: false,
+    },
+    // ==========================================
+    // CAMPOS CONDICIONALES (NULLABLE A NIVEL BD)
+    // ==========================================
+    segundoApellido: {
+      type: 'varchar',
+      length: 100,
+      nullable: true, 
+    },
+    fechaNacimiento: {
+      type: 'date',
+      nullable: true,
+    },
+    email: {
+      type: 'varchar',
+      length: 200,
+      nullable: true,
+    },
+    codigoCiudad: { 
+      type: 'int',
+      nullable: true,
+    }
+  },
+  relations: {
+    ciudad: {
+      target: 'Ciudad',
+      type: 'many-to-one',
+      joinColumn: { name: 'codigoCiudad' },
+      onDelete: 'RESTRICT',
+    },
+  },
 });
