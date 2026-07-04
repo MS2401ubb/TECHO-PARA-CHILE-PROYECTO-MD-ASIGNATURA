9ffdd4b5513ecd86fd734acfad84fcda1b8c5c99
MS2401ubb <mauricio.sandoval2401@alumnos.ubiobio.cl>
2026-07-04 17:40:39 -0400

    funciones generarToken para Jefe de Cuadrilla

--- backend/src/services/cuadrilla.service.js
+++ backend/src/services/cuadrilla.service.js
@@ -4,7 +4,8 @@ import Voluntario from "../entities/voluntario.entity.js";
 import VoluntarioParticipaEnCuadrilla from "../entities/voluntarioParticipaEnCuadrilla.entity.js";
 import JefeCuadrilla from "../entities/jefeCuadrilla.entity.js";
 import JefeCuadrillaLideraCuadrilla from "../entities/jefeCuadrillaLideraCuadrilla.entity.js";
-import { IsNull } from "typeorm";
+import TokenAsignaCuadrilla from "../entities/tokenCuadrilla.entity.js";
+import { IsNull, Not} from "typeorm";
 
 
 // ALL
@@ -220,3 +221,109 @@ export async function asignarJefeCuadrillaACuadrillaService(rutJefeCuadrilla, co
     await queryRunner.release();
   }
 }
+
+/*
+Jefe de Cuadrilla: "Generar Código de Terreno" -> POST /API/cuadrillas/:codigo/token -> recibe token
+
+*/
+
+/**
+ * FUNCION INTERNA
+ * se ejecuta solo si obtenerToken no encuentra uno activo 
+ * 
+ * @param {Repository<TokenAsignaCuadrilla>}tokenRepository - repositorio de tokens 
+ */
+
+async function generarTokenCuadrillaExpress (codigoCuadrillaNumero,tokenRepository){
+    const codigoAleatorio = await generarTokenUnicoString(tokenRepository,codigoCuadrillaNumero);
+    
+    const nuevoToken = tokenRepository.create({
+      codigoCuadrilla: codigoCuadrillaNumero,
+      valorToken: codigoAleatorio,
+      activo: true,
+      fechaCreacion: new Date()
+    });
+    
+    return await tokenRepository.save(nuevoToken);
+}
+
+export async function obtenerToken(rutJefe,codigoCuadrilla){
+  const codigoCuadrillaNumero = Number(codigoCuadrilla);
+
+  if (!Number.isInteger(codigoCuadrillaNumero) || codigoCuadrillaNumero <= 0) {
+    throw new Error("El codigo de cuadrilla debe ser un número entero positivo.");
+  }
+
+  const jefeCuadrillaRepository = AppDataSource.getRepository(JefeCuadrilla);
+  const liderazgoRepository = AppDataSource.getRepository(JefeCuadrillaLideraCuadrilla);
+  const tokenRepository = AppDataSource.getRepository(TokenAsignaCuadrilla);
+
+  try{
+
+    const jefeCuadrilla = await jefeCuadrillaRepository.findOne({
+      where: { rutUsuario: rutJefe },
+    });
+    if (!jefeCuadrilla) {
+      throw new Error("Jefe de Cuadrilla no encontrado.");
+    }
+
+    const cuadrilla = await cuadrillaRepository.findOne({
+      where: { codigo: codigoCuadrillaNumero },
+    });
+    if (!cuadrilla) {
+      throw new Error("Cuadrilla no encontrada.");
+    }
+
+    const liderazgoActivoJefe = await liderazgoRepository.findOne({
+      where: {
+        rutJefeCuadrilla: rutJefe,
+        codigoCuadrilla: codigoCuadrillaNumero,
+        fechaFin: IsNull(),
+      },
+    });
+    if (!liderazgoActivoJefe) {
+      throw new Error("El usuario no es el jefe activo de esta cuadrilla. No puede generar tokens.");
+    }
+
+    const tokenExistente = await tokenRepository.findOne({
+      where: {
+        codigoCuadrilla: codigoCuadrillaNumero,
+        activo: true,
+      }
+    });
+
+    if(tokenExistente) return tokenExistente;
+
+    return await generarTokenCuadrillaExpress(codigoCuadrillaNumero,tokenRepository);
+
+  }catch (error){
+    throw error;
+  }
+}
+
+/**
+ * Auxiliar que garantiza string generado sea único, sin repetirse en generarTokenCuadrillaExpress 
+ * 
+ * @param {Repository<TokenAsignaCuadrilla>}tokenRepository - repositorio de tokens 
+ */
+
+async function generarTokenUnicoString(tokenRepository,codigoCuadrillaNumero) {
+  const codigoAleatorio = Math.random().toString(36).substring(2,8).toUpperCase();
+
+  const tokenDuplicado = await tokenRepository.findOne({
+    where: {
+      valorToken: codigoAleatorio,
+      activo: true,
+      codigoCuadrilla: Not(codigoCuadrillaNumero),
+    }
+  });
+
+  if(tokenDuplicado) return await generarTokenUnicoString(tokenRepository,codigoCuadrillaNumero);
+  
+  return codigoAleatorio;
+}
+
+//Voluntario: "Unirme a Cuadrilla" (Invitado, solo tiene rut) -> POST /API/cuadrillas/token/canjear -> vinculación inmediata a cuadrilla.
+//asocia idToken a voluntario, crea Usuario y Voluntario (con mayoria de columnas NULL)
+//o si el voluntario ya existe y fue re-asignado, solo actualiza cuadrilla actual (le da fecha-fin, en entities asociadas, etc)
+
