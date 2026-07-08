11425b15e530b345dc8061c0abb2a123e1af7468
MS2401ubb <mauricio.sandoval2401@alumnos.ubiobio.cl>
2026-07-07 23:01:28 -0400

    más parametros en ruta validar token (con rut

--- backend/src/routes/cuadrilla.routes.js
+++ backend/src/routes/cuadrilla.routes.js
@@ -30,7 +30,7 @@ router.get(
 	getCuadrillas
 );
 router.post("/token/canjear",getTokenVoluntario);
-router.get("/token/validar/:token",validarTokenExpress);
+router.get("/token/validar/:token/:rut",validarTokenExpress);
 router.get(
 	"/:codigo",
 	authenticateJwt,
